4e1223645f6d47961db1922230634fc2177d715d
Daniel Milisits <daniel.milisits@outlook.com>
2020-05-11 16:58:44 -0400

    Formatted each favorite list as a separate element, done by splitting at commas

--- frontend/workout-generator/src/components/FavoriteCard.js
+++ frontend/workout-generator/src/components/FavoriteCard.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './styles/FavoriteCard.css';
-import { useState } from 'react';
 
 function FavoriteCard({favorite}) {
 
+    // Since an array of arrays containing long strings separated by commas, 
+    // iterate through the master list and iterate through each element SPLIT 
+    // at its commas.
   return (
     <div className="favorite-card">
       <div className="favorite-card-content">
         <ul className="title-list">
           {favorite.map(title => (
-            <li>{title}</li>
+            title.split(",").map(text => (
+              <li>{text}</li>
+            ))
           ))}
         </ul>
       </div>
